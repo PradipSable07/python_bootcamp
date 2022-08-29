1b58fce0efdd8b9c6f4d4e1b69c50fd1ecc1fc7e
Pradip Sable <sablepradipsubhash@gmail.com>
2022-08-29 11:12:43 +0530

    creating a function

---
+++ 100_days_python/Day 8/createFunction.py
@@ -0,0 +1,17 @@
+#Creating a Function 
+'''
+def function_name():
+    Do this
+    Then do this 
+    Finally do this    
+'''
+#calling a function
+# function_name() 
+# below is the example of function
+def greet():
+    print("Hello")
+    print("How do you do?")
+    print("Are you free Tomorrow?")
+    
+greet()
+
