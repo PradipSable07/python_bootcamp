1f8cf787cd3b9598d2b427d5efe884204578da0e
Pradip Sable <sablepradipsubhash@gmail.com>
2022-08-30 23:54:43 +0530

    functions with more than one parameter

--- 100_days_python/Day 8/createFunction.py
+++ 100_days_python/Day 8/createFunction.py
@@ -22,14 +22,31 @@ def function_name(declear a variable or parameter):
     Do this input or decleared variable or parameter.
     Then do this 
     Finally do this
+    
+#  calling a function()
+ function_name(input or Declare a variable value or argument)
 '''
-# calling a function()
-# function_name(input or Declare a variable value or argument)
 
 # Functions that allows for input 
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}? ")
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}? ")
 
-greet_with_name("Pradip_Sable")
+# greet_with_name("Pradip_Sable")
+    
+# Functions with more than one parameter 
+
+
+def greet_with(name,locations):
+    print(f"Hello {name}")
+    print(f"What is it like in {locations}?")
+    
+# greet_with("Pradip Sable","India")
+# vs
+# greet_with("India","Pradip Sable")
+
+# Function with keyword arguments
+greet_with(locations="India",name="vasudev")
+
+
     
