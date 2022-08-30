9ba1cfc0ba613e0c960f20ac4c1ba286d8f79ef9
Pradip Sable <sablepradipsubhash@gmail.com>
2022-08-30 23:24:38 +0530

    functions with input

--- 100_days_python/Day 8/createFunction.py
+++ 100_days_python/Day 8/createFunction.py
@@ -8,10 +8,20 @@ def function_name():
 #calling a function
 # function_name() 
 # below is the example of function
-def greet():
-    print("Hello")
-    print("How do you do?")
-    print("Are you free Tomorrow?")
+# def greet():
+#     print("Hello")
+#     print("How do you do?")
+#     print("Are you free Tomorrow?")
     
-greet()
+# greet()
 
+# Creating a Function with input
+
+'''
+def function_name(declear a variable or parameter):
+    Do this input or decleared variable or parameter.
+    Then do this 
+    Finally do this
+'''
+# calling a function()
+# function_name(input or Declare a variable value or argument)
