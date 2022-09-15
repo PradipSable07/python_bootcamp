5abb6266f65b720142dbaaf86b94005b1c12220a
Pradip Sable <sablepradipsubhash@gmail.com>
2022-09-15 13:58:26 +0530

    complited exerices

--- 100_days_python/Day 10/exerices_01.py
+++ 100_days_python/Day 10/exerices_01.py
@@ -25,15 +25,24 @@ def is_leap(year):
 
 def days_in_month(year, month):
     if month > 12 or month < 1:
-        return "Invalid month😵"
+        return "Invalid input😵"
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     if is_leap(year) and month == 2:
-      return 29
-    else:
-        return month_days[month - 1]
+        return 29
+    return month_days[month - 1]
   
  
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
 print(days)
+
+    # if month > 12 or month < 1:
+    #     return "Invalid month😵"
+    # month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    # if is_leap(year) and month == 2:
+    #   return 29
+    # else:
+    #     return month_days[month - 1]
+    
+    
